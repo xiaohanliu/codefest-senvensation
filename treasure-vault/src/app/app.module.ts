6a73082d20d344eae097cf82db3313a94d035f03
XHL <liuxiaohan01@gmail.com>
2021-09-21 10:48:47 -0500

    add search problems page

--- treasure-vault/src/app/app.module.ts
+++ treasure-vault/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchProblemsComponent } from './search-problems/search-problems.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchProblemsComponent
   ],
   imports: [
     BrowserModule,
---
+++ treasure-vault/src/app/search-problems/search-problems.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-problems',
+  templateUrl: './search-problems.component.html',
+  styleUrls: ['./search-problems.component.scss']
+})
+export class SearchProblemsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
